778bb5b86596c8bfb806eb0c1457908b31441cb8
Leontovych <Kali@Germans-MacBook-Pro.local>
2017-12-05 15:57:34 -0500

    edited navbar with team

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,11 +14,9 @@ class Navbar extends Component {
               <a className="brand-logo left"><img id="logo" src="https://scontent-lga3-1.xx.fbcdn.net/v/t35.0-12/23432792_10210086931114078_260728260_o.jpg?oh=5d6cf317fab2547ee3b8db7a672e0caf&oe=5A24CEC4"/></a>
               <ul className="right hide-on-med-and-down">
                 <li><a onClick={() => this.props.history.push('/')}>Home</a></li>
-                <li><a onClick={() => this.props.history.push('/aboutus')}>About Us</a></li>
-                <li><a>Adopt</a></li>
+                <li><a onClick={() => this.props.history.push('/team')}>Team</a></li>
+                <li><a target="_blank" href="https://www.medium.com/sepqc">Blog</a></li>
                 <li><a>Askseptopus</a></li>
-                <li><a href="https://medium.com/sepqc">Blog</a></li>
-                <li><PopOverExampleSimple/></li>
                 <li style={{marginRight: 100}}><a>Connect</a></li>
               </ul>
             </div>
@@ -30,3 +28,5 @@ class Navbar extends Component {
 }
 
 export default withRouter(Navbar);
+
+
